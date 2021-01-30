bd9270b988f4159ac109cc56dd63aab3d9e72c35
Ryoji Uehara <orcinusorca1758dv6932@gmail.com>
2021-01-30 17:14:15 +0900

    fix_bag

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 docker build -t vizque:latest .
 touch vizque.sh
-
+mkdir tmp_result
 vizque_dir=`pwd`
 
 echo "docker run -it --rm -v $vizque_dir:/VizQue --name vizque vizque:latest" > vizque.sh
 echo "file=\`ls $vizque_dir/tmp_result/ | grep svg\`" >> vizque.sh
 echo "open $vizque_dir/tmp_result/\$file"  >> vizque.sh
 
-echo 'alias vizque="sh $vizque_dir/vizque.sh"' >> ~/.zshrc
+
+echo "alias vizque=\"sh $vizque_dir/vizque.sh\"" >> ~/.zshrc
 source ~/.zshrc
 

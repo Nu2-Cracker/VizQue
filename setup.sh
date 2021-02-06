c0e9771b8b78cdafba448ae7d34ad055bd0e5441
Nu2-Cracker <orcinusorca1758dv6932@gmail.com>
2021-02-07 01:08:32 +0900

    change position

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/zsh
 
 docker-compose up --build -d
-docker-compose exec vizque_apps bash
 
 
 
 vizque_dir=`pwd`
 
 #実行シェル
-echo "cd $vizque_dir/vizque" > vizque.sh
-echo "docker-compose exec vizque_apps vizque" > vizque.sh
+echo "cd $vizque_dir" > vizque.sh
+echo "docker-compose exec vizque_apps vizque" >> vizque.sh
 
 
 echo "alias vizque=\"sh $vizque_dir/vizque.sh\"" >> ~/.zshrc
@@ -22,7 +21,7 @@ echo "echo -n \"Can I delete it? [y/N]:\"" > uninstall.sh
 echo "read ans" >> uninstall.sh
 
 echo "if [ \"\$ans\" = \"y\" ]; then" >> uninstall.sh
-echo "  docker stop -f vizque" >> uninstall.sh
+echo "  docker stop vizque" >> uninstall.sh
 echo "  docker rm -f vizque" >> uninstall.sh
 echo "  docker rmi -f vizque_img" >> uninstall.sh
 echo "  cd $vizque_dir/.." >> uninstall.sh
@@ -30,3 +29,4 @@ echo "  rm -rf VizQue" >> uninstall.sh
 echo "  sed -i -e '/alias vizque/d' ~/.zshrc" >> uninstall.sh
 echo "  source ~/.zshrc" >> uninstall.sh
 echo "fi" >> uninstall.sh
+
